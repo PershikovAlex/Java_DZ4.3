f0dd4e862ec344685d88b5499f9310741ab66f8f
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-04-09 12:21:43 +0500

    Add calculate

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 
 public class Main {
-    
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+
+        System.out.println("Ежемесячный платеж - " + service.calculate(1_000_000, 9.99, 12));
+        System.out.println("Ежемесячный платеж - " + service.calculate(1_000_000, 9.99, 24));
+        System.out.println("Ежемесячный платеж - " + service.calculate(1_000_000, 9.99, 36));
+        System.out.println("Ежемесячный платеж - " + service.calculate(2_000_000, 8.99, 24));
+
+    }
 }
